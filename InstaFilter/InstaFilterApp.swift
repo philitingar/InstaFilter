4b4f26de73e47ef9c02e8ebb08b37f33e668eade
Timi <timi@Timis-Air.lan>
2022-11-23 17:47:38 +0100

    first commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Timi on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
